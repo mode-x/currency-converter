b34b665ea9f25e9f5185c8d96a3bb8a003dd12fb
mode-x <abiaemma@gmail.com>
2018-06-28 09:25:01 +0100

    Reviewed SW setup

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'app-cache-v1'
 const urlsToCache = [
-  './index.html',
-  '../assets/css/w3.css',
-  '../assets/css/app.css',
-  '../assets/js/initializer.js',
-  '../assets/js/idb.js',
-  '../assets/js/db.js',
-  '../assets/js/converter.js',
+  '/',
+  '/assets/css/w3.css',
+  '/assets/css/app.css',
+  '/assets/js/initializer.js',
+  '/assets/js/idb.js',
+  '/assets/js/db.js',
+  '/assets/js/converter.js',
   'https://fonts.googleapis.com/css?family=Gothic+A1',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
 ]
