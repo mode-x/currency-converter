4057a0209a0ccae02ec8931033070813ec977c85
mode-x <abiaemma@gmail.com>
2018-06-28 21:15:25 +0100

    Reviewed SW

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'app-cache-v1'
 const urlsToCache = [
-  'app_shell/index.html',
+  'app_shell/',
   'assets/css/w3.css',
   'assets/css/app.css',
   'assets/js/initializer.js',
@@ -25,7 +25,7 @@ self.addEventListener('fetch', (event) => {
   let requestUrl = new URL(event.request.url)
   if (requestUrl.origin === location.origin) {
     if (requestUrl.pathname === '/') {
-      event.respondWith(caches.match('app_shell/index.html'))
+      event.respondWith(caches.match('app_shell/'))
       return
     }
   }
